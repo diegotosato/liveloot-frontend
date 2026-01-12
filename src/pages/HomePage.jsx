e677a79edf51ecc260e081aeabd21c6cd0995727
MicheleTodeschini <micheletodeschini01@gmail.com>
2026-01-12 18:40:26 +0100

    W ambrogio

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,25 +1,40 @@
 import SwiperComponent from "../components/SwiperComponent"
 import video from "../assets/img/video-stanza.gif"
+import scritta from "../assets/img/scritta-logo.png"
 
 export default function HomePage() {
     return (
         <>
             <div className="jumbotron">
                 <img src={video} className="video"></img>
-                <h2 className="bottino"> <span className="scritta">Il</span> <span className="bocchino">bottino</span> <span className="scritta">perfetto per il tuo </span> <span className="stream">STREAM</span></h2>
+                <div className="bottino">
+                    <img src={scritta}></img>
+                </div>
             </div>
             <div className="sfondo">
                 <section className="best-seller">
+
                     <div className="palla viola palla-grande "></div>
                     <div className="palla rosso palla-media "></div>
                     <div className="palla azzurro palla-piccola "></div>
+                    <div className="palla rosso palla-piccola "></div>
 
                     <div className="container">
+
                         <div className="card mt-3" >
-                            <div className="palla rosso palla-piccola "></div>
-                            <h2 className="mt-2">
-                                <span>i più</span> venduti
-                            </h2>
+
+                            <div className="wave">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </div>
+
+                            <div className="content">
+                                <h2 className="mt-2">
+                                    <span>i più</span> venduti
+                                </h2>
+                            </div>
+
                             <div className="card-body">
                                 <SwiperComponent />
                             </div>
