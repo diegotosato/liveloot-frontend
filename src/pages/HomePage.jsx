822fd3406ddd948db99b6b12a3b5e3dd081a8091
MicheleTodeschini <micheletodeschini01@gmail.com>
2026-01-12 16:19:37 +0100

    Add video background to HomePage and style adjustments

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,25 @@
 import SwiperComponent from "../components/SwiperComponent"
+import video from "../assets/img/video-stanza.gif"
 
 export default function HomePage() {
     return (
-        <div className="container">
-            <div className="card mt-3" >
-                <h2 className="mt-2">
-                    <span>i più</span> venduti
-                </h2>
-                <div className="card-body">
-                    <SwiperComponent />
+        <>
+            <div className="jumbotron">
+                <img src={video} className="video"></img>
+                <h2 className="bottino"> <span className="scritta">Il</span> <span className="bocchino">bottino</span> <span className="scritta">perfetto per il tuo </span> <span className="stream">STREAM</span></h2>
+            </div>
+
+            <div className="container">
+                <div className="card mt-3" >
+                    <h2 className="mt-2">
+                        <span>i più</span> venduti
+                    </h2>
+                    <div className="card-body">
+                        <SwiperComponent />
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 };
 
