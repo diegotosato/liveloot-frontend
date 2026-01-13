3c20574b605168814fd10851ad180d0819c6aa8f
diegotosato <tosato1998@gmail.com>
2026-01-13 18:32:58 +0100

    ambrogio sa fare stile

--- src/components/SingleItemCard.jsx
+++ src/components/SingleItemCard.jsx
@@ -51,48 +51,53 @@ export default function SingleItemCard() {
     return (
         <>
             <div className="back-gradient">
+
                 <section className="product-section best-seller">
+
                     <div className="sphere sphere-purple sphere-bigger"></div>
                     <div className="sphere sphere-red sphere-medium"></div>
                     <div className="sphere sphere-blue sphere-smaller"></div>
                     <div className="sphere sphere-red sphere-smaller "></div>
 
-                    <div className="container py-4 ">
+                    <div className="container">
 
-                        <div className="card border-0 d-flex ">
-                            <div className=" container row" key={singleProduct.id}>
-                                <div className="card-left col-7 py-3 ml-3">
-                                    <img src={`http://localhost:3000/${singleProduct.image}`} alt="" />
-                                </div>
-                                <div className="card-right col-5">
-                                    <h2>{singleProduct.title}</h2>
-                                    <p>Marca: {singleProduct.brand}</p>
-                                    <p>{singleProduct.description}</p>
-                                    <p>Prezzo: € {updatePrice(singleProduct.price)}</p>
-                                    <section className="d-flex align-items-center gap-2">
-                                        <p className="mb-0">Quantità:</p>
+                        <div className="card border-0 px-3">
 
-                                        <button className="aggiunta-rimozione" onClick={handleSub}>−</button>
-                                        <form onSubmit={(e) => handleSubmit(e)}>
-                                            <input
-                                                type="number"
-                                                className="number-quantity"
-                                                value={quantity}
-                                                onChange={(e) => setQuantity(e.target.value)}
-                                            />
-                                        </form>
+                            <div className="container" key={singleProduct.id}>
+                                <div className="row row-cols-2 g-5 p-5">
 
+                                    <div className="prod-image col py-3">
+                                        <img src={`http://localhost:3000/${singleProduct.image}`} alt="" />
+                                    </div>
 
-                                        <button className="aggiunta-rimozione" onClick={handleAdd}>+</button>
-                                    </section>
-                                    <section className="d-flex">
-                                        <div className="row">
-                                            <button className="btn btn-back rounded-pill  col-5 m-2" onClick={() => prevPage()}>TORNA INDIETRO</button>
-                                            <button className="btn btn-add-to-cart rounded-pill  col-5 m-2">AGGIUNGI AL CARRELLO</button>
-                                            <button className="btn btn-avanti-indietro rounded-pill bg-light  col-5 m-2">PRODOTTO PRECEDENTE</button>
-                                            <button className="btn bg-light rounded-pill  btn-avanti-indietro  col-5 m-2">PRODOTTO SUCCESSIVO</button>
-                                        </div>
-                                    </section>
+                                    <div className="prod-details col">
+
+                                        <h3 className="prod-title">{singleProduct.title}</h3>
+                                        <p className="mb-0">Marca: <span className="prod-brand">{singleProduct.brand}</span></p>
+                                        <p>{singleProduct.description}</p>
+                                        <p>Prezzo: <strong className="prod-price">€ {updatePrice(singleProduct.price)}</strong></p>
+
+                                        <section className="d-flex align-items-center">
+                                            <p className="mb-0">Quantità:</p>
+                                            <button className="aggiunta-rimozione" onClick={handleSub}>−</button>
+                                            <form onSubmit={(e) => handleSubmit(e)}>
+                                                <input
+                                                    type="number"
+                                                    className="number-quantity"
+                                                    value={quantity}
+                                                    onChange={(e) => setQuantity(e.target.value)} />
+                                            </form>
+                                            <button className="aggiunta-rimozione" onClick={handleAdd}>+</button>
+                                        </section>
+
+                                        <section className="prod-buttons">
+                                            <button className="btn-back rounded-pill" onClick={() => prevPage()}>TORNA INDIETRO</button>
+                                            <button className="btn-add-to-cart rounded-pill">AGGIUNGI AL CARRELLO</button>
+                                            <button className="btn-avanti-indietro rounded-pill">PRODOTTO PRECEDENTE</button>
+                                            <button className="btn-avanti-indietro rounded-pill">PRODOTTO SUCCESSIVO</button>
+                                        </section>
+
+                                    </div>
                                 </div>
                             </div>
                         </div>
