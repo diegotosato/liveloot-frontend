f5d9165d09fa0f1375f55ae7d458c9b7f7717e3c
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2026-01-14 20:08:38 +0100

    Ambrogio ha spinto il carrello ha fatto l'ultimo sforzo e si è accasciato dalla stanchezza

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -28,6 +28,31 @@ export default function Cart() {
                     </div>
                 </div>
             </div>
+            {/* contenitore totale pagamento */}
+            <div className="summary-card">
+                <div className="cart-summary-card">
+                    <div className="subtotale">
+                        <span className="title-subtotale">Subtotale</span>
+                        <span className="price-subtotale">€ 00.00</span>
+                    </div>
+                    <div className="shipment">
+                        <span className="title-shipment">Spedizione</span>
+                        <span className="price-shipment">€ 00.00</span>
+                    </div>
+                    <p className="shipment-text">
+                        spedizione gratuita
+                    </p>
+                    <div className="total-summary">
+                        <span className="total-title">Totale</span>
+
+                        <div className="final-price">
+                            <span className="total-price">€ 00.00</span>
+                            <button className="order-button">PROCEDI ALL'ORDINE</button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </section>
     );
 }
