34e355116ea99107377c9b876830618739210b01
MicheleTodeschini <micheletodeschini01@gmail.com>
2026-01-19 14:07:31 +0100

    added payment processing components with Stripe integration, including PaymentLayout and CheckoutForm, and updated App structure to include CartProvider

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,40 @@ import CategorieProdotti from "./pages/CategorieProdotti"
 import FocusProdotto from "./pages/FocusProdotto"
 import AllProdotti from "./pages/AllProdotti"
 import { GlobalProvider } from "./context/GlobalContext"
+import { CartProvider } from "./context/CartContext"
 import ScrollToTop from "./components/ScrollToTop"
 import Cart from "./pages/Cart"
 import PaymentSection from './pages/PaymentSection'
 import NotFound from "./pages/NotFound"
+import PaymentLayout from "./Layout/PaymentLayout"
+import CompletePage from "./pages/CompletePage"
+
 
 
 function App() {
   return (
     <>
       <GlobalProvider>
-        <BrowserRouter>
-          <ScrollToTop />
-          <Routes>
-            <Route element={<DefaultLayout />} >
-              <Route path="/" element={<HomePage />} />
-              <Route path="/categorie" element={<Categorie />} />
-              <Route path="/categorie/:slug" element={<CategorieProdotti />} />
-              <Route path="/categorie/:slug/:slug_product" element={<FocusProdotto />} />
-              <Route path="/prodotti" element={<AllProdotti />} />
-              <Route path="/carrello" element={<Cart />} />
-              <Route path="/carrello/pagamento" element={<PaymentSection />} />
-              <Route path="*" element={<NotFound />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
+        <CartProvider>
+          <BrowserRouter>
+            <ScrollToTop />
+            <Routes>
+              <Route element={<DefaultLayout />} >
+                <Route path="/" element={<HomePage />} />
+                <Route path="/categorie" element={<Categorie />} />
+                <Route path="/categorie/:slug" element={<CategorieProdotti />} />
+                <Route path="/categorie/:slug/:slug_product" element={<FocusProdotto />} />
+                <Route path="/prodotti" element={<AllProdotti />} />
+                <Route path="/carrello" element={<Cart />} />
+                <Route element={<PaymentLayout />} >
+                  <Route path="/carrello/pagamento" element={<PaymentSection />} />
+                  <Route path="/PagamentoCompletato" element={<CompletePage />}></Route>
+                </Route>
+                <Route path="*" element={<NotFound />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </CartProvider>
       </GlobalProvider>
     </>
   );
